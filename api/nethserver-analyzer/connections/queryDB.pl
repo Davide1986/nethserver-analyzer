20ed02d9438c250153c85ba1785128213f349f04
Davide De Rubeis <davide.derubeis@gmail.com>
2023-06-19 19:36:37 +0200

    Aggiornato script perl che recupera info di un IP

--- api/nethserver-analyzer/connections/queryDB.pl
+++ api/nethserver-analyzer/connections/queryDB.pl
@@ -1,3 +1,5 @@
+  GNU nano 2.3.1                                                  File: queryDB.pl
+
 #!/usr/bin/perl
 
 use strict;
@@ -8,7 +10,7 @@ sub get_ip_score {
     my $ip_address = shift;
 
     # Path completo dello script bash
-    my $script_path = '/usr/libexec/nethserver/api/nethserver-analyzer/connections/abuseDB_score.sh';
+    my $script_path = '/usr/libexec/nethserver/api/nethserver-analyzer/connections/abuseDb_scoreV1.sh';
 
     # Comando per eseguire lo script bash con l'indirizzo IP come argomento
     my $command = "$script_path $ip_address";
@@ -23,17 +25,18 @@ sub get_ip_score {
     my $json = decode_json($output);
 
     # Recupera i valori desiderati
-    my $abuseConfidenceScore = $json->{data};
-#    my $countryCode = $json->{data}->{countryCode};
-#    my $isWhitelisted = $json->{data}->{isWhitelisted};
+    my $abuseConfidenceScore = $json->{data}->{abuseConfidenceScore} // 'N/A';
+    my $countryCode = $json->{data}->{countryCode} // 'N/A';
+    my $isWhitelisted = $json->{data}->{isWhitelisted} // 'N/A';
 
     # Stampa i valori
     print "abuseConfidenceScore: $abuseConfidenceScore\n";
-#    print "countryCode: $countryCode\n";
-#    print "isWhitelisted: $isWhitelisted\n";
+    print "countryCode: $countryCode\n";
+    print "isWhitelisted: $isWhitelisted\n";
 
 }
 
 # Chiamata alla funzione con l'indirizzo IP desiderato
 my $ip_address = '8.8.4.4';
 get_ip_score($ip_address);
+
